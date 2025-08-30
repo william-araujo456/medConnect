56ee01a3c2a7f1fcb62e50b89fa398098fcaabb8
William Mattson Kheel Gomes Araújo <william.mattson02@gmail.com>
2025-08-29 20:11:19 -0400

    fix migrates

--- hospital/migrations/0002_unaccent.py
+++ hospital/migrations/0002_unaccent.py
@@ -2,7 +2,7 @@ from django.db import migrations
 
 class Migration(migrations.Migration):
     dependencies = [
-        ('hospital', '0002_alter_medication_type_application'),
+        ('hospital', '0001_initial'),
     ]
 
     operations = [
